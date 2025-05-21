a00b32d88a16ebedddab0761c952745bbc6c0dd0
Parsa <parsaes1386@gmail.com>
2025-05-21 20:41:44 +0330

    fixed a bracket

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import HTTPhandler.UserHTTPHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.net.InetSocketAddress;
@@ -7,11 +8,13 @@ import java.net.InetSocketAddress;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080),0);
-
-        }
+        server.createContext("/auth/register",new UserHTTPHandler());
+        server.createContext("/auth/login",new UserHTTPHandler());
+        server.start();
+        System.out.println("Server started on http://localhost:8080");
     }
 }

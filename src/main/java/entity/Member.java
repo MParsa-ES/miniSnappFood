636a588211a488f61fd3262abb27a1e849439e2e
Parsa <parsaes1386@gmail.com>
2025-05-21 19:16:24 +0330

    added the columns for member class and the first step for creating a http server

--- src/main/java/entity/Member.java
+++ src/main/java/entity/Member.java
@@ -1,6 +1,8 @@
 package entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 enum Role {
     BUYER, SELLER, COURIER
@@ -12,12 +14,40 @@ enum Role {
 @Setter
 @Table(name = "Users")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = false, nullable = false)
     private String name;
+
+    @Column(unique = false, nullable = false)
     private String familyName;
+
+    @Column(unique = true, nullable = false)
     private String phone;
+
+    @Column(unique = false, nullable = true)
     private String email;  // optional
+
+    @Column(unique = false, nullable = false)
     private String password;
+
+    @Column(unique = false, nullable = false)
     private Role role;
+
+    @Column(unique = false, nullable = false)
     private String address;
 
+
+    public Member(String name, String familyName, String phone, String email, String password, Role role, String address) {
+        this.name = name;
+        this.familyName = familyName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.address = address;
+    }
 }
